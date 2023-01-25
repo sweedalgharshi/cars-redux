c66d1d7120dfca9bc4dcb61d91a72045aa970328
sweedalgharshi <sweedalgracius@gmail.com>
2023-01-25 15:57:26 +0530

    highlighting existing cars

--- src/component/CarList.js
+++ src/component/CarList.js
@@ -4,8 +4,15 @@ import { removeCar } from "../store";
 function CarList() {
   const dispatch = useDispatch();
 
-  const cars = useSelector(({ cars: { data, searchTerm } }) => {
-    return data.filter((car) => car.name.toLowerCase().includes(searchTerm.toLowerCase()));
+  const { cars, name } = useSelector(({ form, cars: { data, searchTerm } }) => {
+    const filteredCars = data.filter((car) =>
+      car.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    return {
+      cars: filteredCars,
+      name: form.name,
+    };
   });
 
   const handleClickDelete = (car) => {
@@ -14,8 +21,9 @@ function CarList() {
   };
 
   const renderedCars = cars.map((car) => {
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
     return (
-      <div key={car.id} className="panel">
+      <div key={car.id} className={`panel ${bold && "bold"}`}>
         <p>
           {car.name} - ${car.cost}
         </p>
